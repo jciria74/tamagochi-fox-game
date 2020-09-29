23323d8f9f04aae33129591fa0046e976ff02eae
jciria74 <jciria74@gmail.com>
2020-09-29 11:54:59 +0200

    game ended

---
+++ src/init.js
@@ -0,0 +1,26 @@
+import gameState from "./gameState";
+import {TICK_RATE} from "./constants";
+import initButtons from "./buttons";
+import game, { handleUserAction } from "./gameState";
+
+
+async function init() {
+  console.log("starting game");
+  initButtons(handleUserAction);
+
+  let nextTimeToTick = Date.now();
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      gameState.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  nextAnimationFrame();
+}
+
+init()
